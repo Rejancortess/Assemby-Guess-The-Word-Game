21b55fcaca7cf9d33eea2501dc31caa1ecfe2b60
JanYuro <rejanmamugay35@gmail.com>
2025-12-02 11:53:42 +0800

    Update README.md

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
   return (
     <header className="mt-10 flex flex-col items-center">
       <h1 className="mb-3 text-4xl text-[#F9F4DA]">Assembly: Guess the word</h1>
-      <h2 className="max-w-lg text-2xl text-[#8E8E8E]">
+      <h2 className="max-w-lg text-center text-2xl text-[#8E8E8E]">
         Guess the word in under 8 attempts to keep the programming world safe from Assembly!
       </h2>
     </header>
